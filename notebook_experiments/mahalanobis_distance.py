ba6a13aa093366d9d9f970a0daf15cc3a5ba3ed0
Taylor Denouden <taylor.denouden@hakai.org>
2019-06-18 12:35:29 -0400

    Update LLR code

---
+++ notebook_experiments/mahalanobis_distance.py
@@ -0,0 +1,77 @@
+class Mahal_Dist(object):
+    def __init__(self, gt_features=None, gt_labels=None):
+        super().__init__()
+        
+        if gt_features is not None and gt_labels is not None:
+            self.fit(gt_features, gt_labels)
+        
+    def fit(self, gt_features, gt_labels):
+        """Fit parameters used for Mahalanobis distance."""
+        self.labels = sorted(np.unique(gt_labels).astype(np.int))
+        
+        mahal_means = []
+        print("Calculating mahal mean")
+        for l in self.labels:
+            mask = gt_labels == l
+            mahal_means.append(gt_features[mask].mean(axis=0))#, dtype=np.float16))
+            
+        self.mahal_means = np.array(mahal_means)
+
+        x = gt_features.copy()
+        print("Calculating mahal cov")
+        for i, l in enumerate(self.labels):
+            mask = gt_labels == l
+            x[mask] -= self.mahal_means[i]
+
+        self.mahal_cov = np.dot(x.T, x) / len(gt_features)
+        print("Inverting")
+        self.inv_mahal_cov = np.linalg.inv(self.mahal_cov + np.eye(len(self.mahal_cov)) * 1e-10)
+        
+    def __call__(self, features):
+        """Return the distance based confidence score to each 
+            of the means."""
+        x =  -self.mahal_means + features
+        m_x = np.diag(-np.dot(np.dot(x, self.inv_mahal_cov), x.T))
+        return np.array(m_x)
+    
+    def label_dist(self, features):
+        """Return the label of the closest mean and the 
+            mahalanobis distance to it."""
+        m_x = self(features)
+        max_i = np.argmax(m_x, axis=0)
+        return np.array([self.labels[max_i], m_x[max_i]])
+    
+    def label(self, features):
+        """Return the label of the closest mean."""
+        return self.labels[np.argmax(self(features), axis=0)]
+    
+    def dist(self, features):
+        """Return the dist of the closest mean."""
+        return np.max(self(features), axis=0)
+    
+    def save(self, fname='mahal_distance.h5'):
+        """Save precomputed Mahal_Dist object with all means and covariance matrix."""
+        with h5py.File(fname, 'a') as f:
+            # Remove old data if exists
+            if "mahal_distance" in f.keys():
+                del f['mahal_distance']
+                
+            # Store the new data
+            g = f.create_group("mahal_distance")
+            g.create_dataset("means", data=self.mahal_means)
+            g.create_dataset("cov", data=self.mahal_cov)
+            g.create_dataset("inv_cov", data=self.inv_mahal_cov)
+            g.create_dataset("labels", data=self.inv_mahal_cov)
+            
+    @classmethod
+    def load(cls, fname='mahal_distance.h5'):
+        """Load precomputed Mahal_Dist object with all means and covariance matrix."""
+        inst = cls()
+        
+        with h5py.File(fname, 'r') as f:
+            inst.labels = f['mahal_distance/labels'][()]
+            inst.mahal_means = f['mahal_distance/means'][()]
+            inst.mahal_cov = f['mahal_distance/cov'][()]
+            inst.inv_mahal_cov = f['mahal_distance/inv_cov'][()]
+        
+        return inst
