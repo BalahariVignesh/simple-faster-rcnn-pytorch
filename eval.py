e474d16de86fd2b730d672c029d059ded0120f26
Taylor Denouden <taylor.a.denouden@uwaterloo.ca>
2019-12-16 11:13:24 -0500

    Add evaluation scripts

---
+++ eval.py
@@ -0,0 +1,78 @@
+from __future__ import  absolute_import
+# though cupy is not used but without this line, it raise errors...
+import os
+
+import ipdb
+import matplotlib
+from tqdm import tqdm
+
+from utils.config import opt
+from data.dataset import Dataset, TestDataset, inverse_normalize
+from model import FasterRCNNVGG16, FasterRCNNVGG19BN
+from torch.utils import data as data_
+from trainer import FasterRCNNTrainer
+from utils import array_tool as at
+from utils.eval_tool import eval_detection_voc
+
+# fix for ulimit
+# https://github.com/pytorch/pytorch/issues/973#issuecomment-346405667
+# import resource
+
+# rlimit = resource.getrlimit(resource.RLIMIT_NOFILE)
+# resource.setrlimit(resource.RLIMIT_NOFILE, (20480, rlimit[1]))
+
+matplotlib.use('agg')
+
+
+def eval(dataloader, faster_rcnn, test_num=10000):
+    pred_bboxes, pred_labels, pred_scores = list(), list(), list()
+    gt_bboxes, gt_labels, gt_difficults = list(), list(), list()
+    for ii, (imgs, sizes, gt_bboxes_, gt_labels_, gt_difficults_) in tqdm(enumerate(dataloader), total=test_num):
+        sizes = [sizes[0][0].item(), sizes[1][0].item()]
+        pred_bboxes_, pred_labels_, pred_scores_ = faster_rcnn.predict(imgs, [sizes])
+        gt_bboxes += list(gt_bboxes_.numpy())
+        gt_labels += list(gt_labels_.numpy())
+        gt_difficults += list(gt_difficults_.numpy())
+        pred_bboxes += pred_bboxes_
+        pred_labels += pred_labels_
+        pred_scores += pred_scores_
+        if ii == test_num: break
+
+    result = eval_detection_voc(
+        pred_bboxes, pred_labels, pred_scores,
+        gt_bboxes, gt_labels, gt_difficults,
+        use_07_metric=False)
+    return result
+
+
+
+def train(**kwargs):
+    opt._parse(kwargs)
+
+    print('load data')
+    testset = TestDataset(opt)
+    test_dataloader = data_.DataLoader(testset,
+                                       batch_size=1,
+                                       num_workers=opt.test_num_workers,
+                                       shuffle=False,
+                                       pin_memory=True
+                                       )
+    if opt.pretrained_model == 'vgg16':
+        faster_rcnn = FasterRCNNVGG16()
+    elif opt.pretrained_model == 'vgg19bn':
+        faster_rcnn = FasterRCNNVGG19BN()
+         
+    print('model construct completed')
+   
+    trainer = FasterRCNNTrainer(faster_rcnn).cuda()
+    trainer.load(opt.load_path)
+    print('load pretrained model from %s' % opt.load_path)
+        
+        
+    eval_result = eval(test_dataloader, faster_rcnn, test_num=len(testset))
+    print(eval_result)
+
+if __name__ == '__main__':
+    import fire
+
+    fire.Fire()
